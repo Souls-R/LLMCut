d7406b0222af15a0cd6fb084732c140b2c4f5af1
jingfelix <jingfelix@outlook.com>
2024-09-08 21:04:52 +0800

    feat: basic structure
    Signed-off-by: jingfelix <jingfelix@outlook.com>

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './components/App.js';
 import reportWebVitals from './reportWebVitals';
+import 'react-material-symbols/outlined'; // Place in your root app file. There are also `sharp` and `outlined` variants.
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
