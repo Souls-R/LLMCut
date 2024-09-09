32cd9c11f61121bc880c575d6dd1fe6005735f4f
jingfelix <jingfelix@outlook.com>
2024-09-09 22:57:11 +0800

    fix: dynamic font-size
    Signed-off-by: jingfelix <jingfelix@outlook.com>

--- src/components/FaqBlock.js
+++ src/components/FaqBlock.js
@@ -5,20 +5,20 @@ function FaqBlock({ className, tKey }) {
     const { t, i18n } = useTranslation();
 
     const lang = i18n.language;
-    let textLarge = '32px';
-    let textSmall = '20px';
+    let textLarge = 'text-[32px]';
+    let textSmall = 'text-[20px]';
     let leading = 'leading-[1.5]';
 
     if (lang === 'en') {
-        textLarge = '40px';
-        textSmall = '24px';
+        textLarge = 'text-[40px]';
+        textSmall = 'text-[24px]';
         leading = 'leading-[1.1]';
     }
 
     return (
         <div className={`flex flex-col justify-between w-blockLarge h-blockLarge rounded-[75px] p-[36px] ${leading} ${className}`} lang={lang}>
-            <p className={`text-[${textLarge}]`}>{t(`${tKey}.title`)}</p>
-            <p className={`text-[${textSmall}]`}>{t(`${tKey}.text`)}</p>
+            <p className={`${textLarge}`}>{t(`${tKey}.title`)}</p>
+            <p className={`${textSmall}`}>{t(`${tKey}.text`)}</p>
         </div>
     )
 }
